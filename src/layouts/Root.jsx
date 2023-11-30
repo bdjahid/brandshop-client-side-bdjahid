bab4556eb978d802f8bc1fac9522ae7911fef89c
Your Name <you@example.com>
2023-11-30 21:23:50 +0600

    first commit setup

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../pages/Header/Header";
+import Footer from "../pages/Footer/Footer";
+
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
